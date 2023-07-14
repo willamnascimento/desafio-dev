3b8a516169f3a90e9af327db5963f911a5547a0d
William Nascimento <william.nascimento2010@gmail.com>
2023-07-14 11:22:03 -0300

    created Service

---
+++ desafio-cnab-backend/Desafio.Service/Services/ImportacaoCNABService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using AutoMapper;
+using Desafio.Domain.DTOs.ImportacaoCNAB;
+using Desafio.Domain.Interfaces.Repositories;
+using Desafio.Domain.Interfaces.Services;
+using Desafio.Domain.Responses;
+
+namespace Desafio.Service.Services;
+
+public class ImportacaoCNABService : IImportacaoCNABService
+{
+    private readonly IMapper mapper;
+    private IImportacaoCNABRepository repository;
+
+    public ImportacaoCNABService(IMapper mapper, IImportacaoCNABRepository repository)
+    {
+        this.mapper = mapper;
+        this.repository = repository;
+    }
+
+    public Task<HttpResponse> Create(ImportacaoCNABDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<ImportacaoCNABDto>> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+}
+
